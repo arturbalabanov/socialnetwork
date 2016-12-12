8e639a7d740c6633b359309c2d81523256f3117f
Artur Balabanov <arturbalabanov97@gmail.com>
2016-12-12 13:58:42 +0000

    Delete timeline views file

--- timeline/urls.py
+++ timeline/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
 
 from timeline.api.viewsets import PostViewSet
-from timeline.views import *
 
 urlpatterns = [
     url(r'posts/$', PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='create_post'),
