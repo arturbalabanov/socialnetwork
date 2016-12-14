aa1f92c331eba559dbe24438b811783725190705
Artur Balabanov <arturbalabanov97@gmail.com>
2016-12-14 00:30:44 +0000

    Better permissions for post CRUD actions

--- timeline/api/permissions.py
+++ timeline/api/permissions.py
@@ -1,7 +1,13 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAuthorOrReadOnly(BasePermission):
+class PostPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return request.user.is_authenticated()
+
     def has_object_permission(self, request, view, post):
         if request.method in SAFE_METHODS:
             return True
--- timeline/api/viewsets.py
+++ timeline/api/viewsets.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from timeline.api.permissions import IsAuthorOrReadOnly
+from timeline.api.permissions import PostPermission
 from timeline.api.serializers import PostSerializer
 from timeline.models import Post
 
@@ -10,7 +10,7 @@ from timeline.models import Post
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (PostPermission,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
