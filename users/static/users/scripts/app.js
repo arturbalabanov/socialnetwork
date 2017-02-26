af4970ce094d9e3d2309bd59b1e7de1de0362f86
Artur Balabanov <arturbalabanov97@gmail.com>
2017-02-26 16:06:12 +0200

    Add animation for the alerts.

--- users/static/users/scripts/app.js
+++ users/static/users/scripts/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by artur on 24/02/17.
  */
-var profileApp = angular.module('profileApp', ['djng.urls', 'ui.bootstrap']);
+var profileApp = angular.module('profileApp', ['djng.urls', 'ngAnimate', 'ui.bootstrap']);
 
 profileApp.config(function ($httpProvider) {
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
