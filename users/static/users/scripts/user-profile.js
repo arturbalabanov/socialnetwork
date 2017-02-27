6be3217158169d6ae875a3aa2ceba9fc13ee5ca5
Artur Balabanov <arturbalabanov97@gmail.com>
2017-02-27 10:44:31 +0200

    Clean up the old jQuery code

--- users/static/users/scripts/user-profile.js
+++ users/static/users/scripts/user-profile.js
@@ -3,65 +3,6 @@
  */
 
 $(document).ready(function () {
-    function likePosts() {
-        var $likeButtons = $('.like-button');
-
-        function likePostAjaxRequest($post) {
-            var id = $post.attr('data-post-id');
-
-            var requestData = {
-                'id': id
-            };
-
-            $.post(LIKE_TIMELINE_POST_URL, requestData).success(function (data) {
-                $post.find('.number-of-likes').text(data['number_of_likes']);
-            });
-        }
-
-        $likeButtons.click(function (e) {
-            var $this = $(this);
-            var $post = $this.closest('.timeline-post');
-            likePostAjaxRequest($post);
-        });
-    }
-
-    // ==================================================
-
-    // var postLikesTemplate = Handlebars.compile($('#post-likes-list-template').html());
-    //
-    // function showPostLikes() {
-    //     var $postLikesLinks = $('.post-likes-link');
-    //     var $postLikesModal = $('#post-likes-modal');
-    //     var $allPostsLikes = $('#all-post-likes');
-    //
-    //     function showLikesDialog(postId) {
-    //         // FIXME: hard coded URL, filthy heretic!
-    //         $.get("/timeline/posts/" + postId + "/get-likes/").success(function (data) {
-    //             $allPostsLikes.html(postLikesTemplate({users: data}));
-    //             $postLikesModal.modal('show');
-    //         });
-    //     }
-    //
-    //     $postLikesLinks.click(function (e) {
-    //         var $this = $(this);
-    //         var postId = $this.closest('.timeline-post').attr('data-post-id');
-    //         showLikesDialog(postId);
-    //     })
-    // }
-
-    // ==================================================
-
-    // $('#post-likes-modal').modal('show');
-    // var postTemplate = Handlebars.compile($('#timeline-post-template').html());
-
-    // $.get(POSTS_LIST_URL).done(function (data) {
-    //     $allPosts.append(postTemplate({posts: data}));
-    //     likePosts();
-    //     // showPostLikes();
-    // });
-
-    // ==================================================
-
     var $newPostForm = $('#new-post-form');
     var $allPosts = $('#all-posts');
     var $emptyPostErrorMessage = $('#empty-post-error-message');
