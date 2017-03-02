615d9b87cbfa6489924b0703b92acd3a91f53ac7
Artur Balabanov <arturbalabanov97@gmail.com>
2017-03-02 18:23:42 +0200

    Show post comments in the user profiles

--- timeline/api/serializers.py
+++ timeline/api/serializers.py
@@ -3,14 +3,6 @@ from rest_framework import serializers
 from timeline.models import Post, PostComment
 
 
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
-
-    class Meta:
-        model = Post
-        fields = ('id', 'text', 'author', 'number_of_likes', 'created')
-
-
 class PostCommentSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     post_id = serializers.ReadOnlyField(source='post.id')
@@ -18,3 +10,13 @@ class PostCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostComment
         fields = ('id', 'post_id', 'text', 'author', 'number_of_likes', 'created')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    comments = PostCommentSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'author', 'number_of_likes', 'created', 'comments')
+
